f58ffceb4a3a80c2f8a596229641a564c69946db
Clinton Enyinna <enyinnaclinton@gmail.com>
2020-05-02 02:58:08 -0500

    refactor: remove repeated code

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,46 +1,16 @@
-import Create from './MakeElement';
+import { element } from './MakeElement';
 import About from './About';
 import Contact from './Contact';
 import Menu from './Menu';
 import Home from './Home';
 
-const Main = (() => {
-  const main = Create.element('MAIN');
-
-  const home = () => {
-    main.innerHTML = '';
-    main.appendChild(Home());
-    return main;
-  };
-
-  const about = () => {
-    main.innerHTML = '';
-    main.appendChild(About());
-    return main;
-  };
-  const contact = () => {
-    main.innerHTML = '';
-    main.appendChild(Contact());
-    return main;
-  };
-  const menu = () => {
-    main.innerHTML = '';
-    main.appendChild(Menu());
-    return main;
-  };
-
-  const choose = (page) => {
-    if (page === 'home') {
-      return home();
-    } if (page === 'about') {
-      return about();
-    } if (page === 'contact') {
-      return contact();
-    }
-    return menu();
-  };
-
-  return { choose };
-})();
+const Main = (page) => {
+  const main = element('MAIN');
+  if (page === 'home') main.appendChild(Home());
+  else if (page === 'about') main.appendChild(About());
+  else if (page === 'contact') main.appendChild(Contact());
+  else main.appendChild(Menu());
+  return main;
+};
 
 export default Main;
